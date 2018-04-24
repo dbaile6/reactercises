5852d79755b433aaea88b0127e271c804154e52c
Dylan Bailey <dylanba1@gmail.com>
2018-04-24 13:57:55 -0400

    Created skeleton

---
+++ myecommerce/src/SignInPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SignInPage extends Component {
+  render() {
+    return (
+     <span>Sign in, friend!</span>
+    );
+  }
+}
+
+export default SignInPage;
