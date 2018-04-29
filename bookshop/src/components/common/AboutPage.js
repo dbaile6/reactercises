f2e9964a85b9e557f3d1450bac9f3226df66c9a2
Dylan Bailey <dylanba1@gmail.com>
2018-04-28 20:41:32 -0400

    Bookstore app

---
+++ bookshop/src/components/common/AboutPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Lorem ipsum dolor amet ennui waistcoat adipisicing, mustache helvetica enamel pin fashion axe qui incididunt succulents migas. Echo park dolore YOLO tempor plaid eiusmod pug offal truffaut brunch. Everyday carry ramps sunt chicharrones sustainable commodo umami. Literally heirloom ex irure PBRB, mustache velit dolore kitsch YOLO. Tumblr paleo tbh fam tousled four dollar toast venmo 90's aesthetic quis photo booth.</p>
+    </div>
+  );
+};
+
+export default About
